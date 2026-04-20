b412cedbf92a3e007287a3ee0f3d7660009b53ba
dwikyputra <dwkyputra21@gmail.com>
2026-04-20 18:06:09 +0700

    Update Banner.php

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    //
+    protected $fillable = [
+    'title', 
+    'image', 
+    'link', 
+    'is_active', 
+    'sort_order' // Pastikan ini sort_order, bukan order
+];
 }
