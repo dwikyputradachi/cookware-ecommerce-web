1de31bf6da6fbaf708d50be73213fde0d88918a1
dwikyputra <dwkyputra21@gmail.com>
2026-04-20 16:51:27 +0700

    Mengganti sementara untuk deploy demo

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,46 +10,35 @@ class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Product::query();
-
-    if ($request->filled('search')) {
-        $query->where('name', 'like', '%' . $request->search . '%')
-              ->orWhere('description', 'like', '%' . $request->search . '%');
-    }
-
-    if ($request->filled('category')) {
-        $query->where('category', $request->category);
-    }
-
-    $products = $query->latest()->get();
-
         $banners = \App\Models\Banner::where('is_active', true)->orderBy('order')->get();
+    
         $categoryNames = \App\Models\Product::distinct()->pluck('category')->filter();
-
+    
         $categories = $categoryNames->map(function($name) {
             return [
                 'name' => $name,
-                //Format file: misal "rice-cooker" ganti jadi ini ya "rice-cooker.png"
-                'img' => strtolower(str_replace(' ', '-', $name)) . '.png' 
+                'img' => strtolower(str_replace(' ', '-', $name)) . '.png'
             ];
         });
-
-        $query = \App\Models\Product::query();
-        if ($request->filled('search')) {
-            $query->where('name', 'like', '%' . $request->search . '%');
+    
+        try {
+            $query = Product::query();
+    
+            if ($request->filled('search')) {
+                $query->where('name', 'like', '%' . $request->search . '%')
+                      ->orWhere('description', 'like', '%' . $request->search . '%');
+            }
+    
+            if ($request->filled('category')) {
+                $query->where('category', $request->category);
+            }
+    
+            $products = $query->latest()->get();
+    
+        } catch (\Exception $e) {
+            $products = [];
         }
-
-        if ($request->filled('category')) {
-            $query->where('category', $request->category);
-        }
-
-        $products = $query->latest()->get();
-
+    
         return view('products.index', compact('products', 'categories', 'banners'));
-    }
-    public function show($id)
-    {
-        $product = Product::findOrFail($id);
-        return view('products.show', compact('product'));
     }
 }
